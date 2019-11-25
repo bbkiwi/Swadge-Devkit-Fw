d8d6b29d265737ad9cccd26815a4ffcb9e117357
bbkiwi <staf186@ext.canterbury.ac.nz>
2019-11-25 14:23:02 +1300

    unlocked images and made shock have lower tolerancy

--- firmware/user/modes/mode_color_movement.c
+++ firmware/user/modes/mode_color_movement.c
@@ -1560,7 +1560,7 @@ void ICACHE_FLASH_ATTR cmNewSetup(subMethod_t subMode)
     cmComputeColor = BPM2HUE; //using bpmFromCrossing
     cmLedMethod = RAINBOW;
     cmUseShiftingColorWheel = true;
-    cmShockLimit = 40;
+    cmShockLimit = 15;
     //TODO can extend to give subset to use eg 1,3,5 or 1,4
     //    rather than just how many consecutive
     cmShowNumLeds = USE_NUM_LEDS; // must be <= USE_NUM_LEDS
